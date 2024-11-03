2e69c45a068e89c198d767f2a6bad452be8d8bb1
Khanh Duong Quoc <dqkqdlot@gmail.com>
2024-11-03 18:08:26 +0900

    Lab utilities: regex for find

--- user/find.c
+++ user/find.c
@@ -4,6 +4,8 @@
 #include "kernel/fs.h"
 #include "kernel/fcntl.h"
 
+int match(char*, char*);
+
 void
 find(char *path, char* pat)
 {
@@ -55,7 +57,7 @@ find(char *path, char* pat)
       switch (st.type) {
       case T_DEVICE:
       case T_FILE:
-        if (strcmp(de.name, pat) == 0)
+        if (match(pat, de.name))
           printf("%s\n", fullpath);
         break;
       case T_DIR:
@@ -78,4 +80,44 @@ main(int argc, char *argv[])
   }
   find(argv[1], argv[2]);
   exit(0);
+}
+
+
+int matchhere(char*, char*);
+int matchstar(int, char*, char*);
+
+int
+match(char *re, char *text)
+{
+  if(re[0] == '^')
+    return matchhere(re+1, text);
+  do{  // must look at empty string
+    if(matchhere(re, text))
+      return 1;
+  }while(*text++ != '\0');
+  return 0;
+}
+
+// matchhere: search for re at beginning of text
+int matchhere(char *re, char *text)
+{
+  if(re[0] == '\0')
+    return 1;
+  if(re[1] == '*')
+    return matchstar(re[0], re+2, text);
+  if(re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if(*text!='\0' && (re[0]=='.' || re[0]==*text))
+    return matchhere(re+1, text+1);
+  return 0;
+}
+
+// matchstar: search for c*re at beginning of text
+int matchstar(int c, char *re, char *text)
+{
+  do{  // a * matches zero or more instances
+    if(matchhere(re, text))
+      return 1;
+  }while(*text!='\0' && (*text++==c || c=='.'));
+  return 0;
 }
