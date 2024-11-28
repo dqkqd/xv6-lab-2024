b83c7650a58eba63af0e09413db4bb907563a679
Khanh Duong Quoc <dqkqdlot@gmail.com>
2024-11-28 20:06:44 +0900

    Lab lock: bcache
    Use 13 hashqueue for storing cached buffer to avoid lock contention.
    Drop freelist, though I think we should evict freed buffers to freelist
    while also keep them inside hashqueue. However, this create high lock
    contention in this lab, so I just remove it.

--- kernel/bio.c
+++ kernel/bio.c
@@ -23,15 +23,31 @@
 #include "fs.h"
 #include "buf.h"
 
+#define NQUEUE 13
+
+struct hashqueue {
+  struct spinlock lock;
+  struct buf head;
+};
+int  hashqueue_eq(struct hashqueue *, struct hashqueue *);
+void hashqueue_init(struct hashqueue *);
+void hashqueue_addhead(struct hashqueue *, struct buf *);
+uint hash(uint);
+
 struct {
   struct spinlock lock;
   struct buf buf[NBUF];
-
-  // Linked list of all buffers, through prev/next.
-  // Sorted by how recently the buffer was used.
-  // head.next is most recent, head.prev is least.
-  struct buf head;
+  struct hashqueue hashqueue[NQUEUE];
 } bcache;
+
+struct hashqueue *gethashqueue(uint);
+
+// buf related functions
+int isin_hashqueue(struct buf *);
+void remove_from_hashqueue(struct buf *);
+
+// lock 2 locks at the sametime, with reordering to avoid deadlock
+void lockboth(struct spinlock *, struct spinlock *);
 
 void
 binit(void)
@@ -39,16 +55,16 @@ binit(void)
   struct buf *b;
 
   initlock(&bcache.lock, "bcache");
+  for(int i = 0; i < NQUEUE; i++)
+    hashqueue_init(&bcache.hashqueue[i]);
 
   // Create linked list of buffers
-  bcache.head.prev = &bcache.head;
-  bcache.head.next = &bcache.head;
   for(b = bcache.buf; b < bcache.buf+NBUF; b++){
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
     initsleeplock(&b->lock, "buffer");
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
+    // make sure this buffer is not exist in any hashqueue
+    remove_from_hashqueue(b);
+    // blockno < 0 means we haven't cached it yet
+    b->blockno = -1;
   }
 }
 
@@ -59,33 +75,69 @@ static struct buf*
 bget(uint dev, uint blockno)
 {
   struct buf *b;
+  struct hashqueue *hq = 0, *ohq = 0;
 
-  acquire(&bcache.lock);
-
-  // Is the block already cached?
-  for(b = bcache.head.next; b != &bcache.head; b = b->next){
+  // search from hashqueue first
+  // we don't need bcache.lock for this,
+  // because those buffers with b->blockno = blockno are protected by `hq.lock`
+  hq = gethashqueue(blockno);
+  acquire(&hq->lock);
+  for(b = hq->head.next; b != &hq->head; b = b->next){
     if(b->dev == dev && b->blockno == blockno){
       b->refcnt++;
-      release(&bcache.lock);
+
+      if(!isin_hashqueue(b))
+        panic("bget: buffer must in hashqueue");
+
+      release(&hq->lock);
       acquiresleep(&b->lock);
       return b;
     }
+  }
+  // drop the lock here
+  // so that we can retain the order (bcache.lock then hq.lock)
+  release(&hq->lock);
+
+  acquire(&bcache.lock);
+  // checking newly buffers
+  for(b = bcache.buf; b < bcache.buf+NBUF; b++){
+    if(b->blockno < 0)
+      goto found;
   }
 
+  acquire(&hq->lock);
   // Not cached.
-  // Recycle the least recently used (LRU) unused buffer.
-  for(b = bcache.head.prev; b != &bcache.head; b = b->prev){
-    if(b->refcnt == 0) {
-      b->dev = dev;
-      b->blockno = blockno;
-      b->valid = 0;
-      b->refcnt = 1;
-      release(&bcache.lock);
-      acquiresleep(&b->lock);
-      return b;
-    }
+  for(b = bcache.buf; b < bcache.buf+NBUF; b++){
+    ohq = gethashqueue(b->blockno);
+    // try to lock both of those hashqueue respecting order to avoid deadlock
+    lockboth(&hq->lock, &ohq->lock);
+
+    if(b->refcnt == 0)
+      goto found;
+
+    // we locked ohq, so need to release it
+    if(!hashqueue_eq(ohq, hq))
+      release(&ohq->lock);
   }
   panic("bget: no buffers");
+
+found:
+  b->refcnt = 1;
+  b->valid = 0;
+  b->dev = dev;
+  b->blockno = blockno;
+  hashqueue_addhead(hq, b);
+
+  release(&bcache.lock);
+
+  if(holding(&hq->lock))
+    release(&hq->lock);
+
+  if(holding(&ohq->lock))
+    release(&ohq->lock);
+
+  acquiresleep(&b->lock);
+  return b;
 }
 
 // Return a locked buf with the contents of the indicated block.
@@ -121,33 +173,112 @@ brelse(struct buf *b)
 
   releasesleep(&b->lock);
 
-  acquire(&bcache.lock);
+  // this buffer must exist in some hashqueue
+  struct hashqueue *hq = gethashqueue(b->blockno);
+  acquire(&hq->lock);
+  if(!isin_hashqueue(b))
+    panic("brelse: buffer must exist in hashqueue");
   b->refcnt--;
-  if (b->refcnt == 0) {
-    // no one is waiting for it.
-    b->next->prev = b->prev;
-    b->prev->next = b->next;
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
-  }
-  
-  release(&bcache.lock);
+  release(&hq->lock);
 }
 
 void
 bpin(struct buf *b) {
-  acquire(&bcache.lock);
+  // this buffer must exist in some hashqueue
+  struct hashqueue *hq = gethashqueue(b->blockno);
+  acquire(&hq->lock);
+  if(!isin_hashqueue(b))
+    panic("bpin: buffer must exist in hashqueue");
   b->refcnt++;
-  release(&bcache.lock);
+  release(&hq->lock);
 }
 
 void
 bunpin(struct buf *b) {
-  acquire(&bcache.lock);
+  // this buffer must exist in some hashqueue
+  struct hashqueue *hq = gethashqueue(b->blockno);
+  acquire(&hq->lock);
+  if(!isin_hashqueue(b))
+    panic("bunpin: buffer must exist in hashqueue");
   b->refcnt--;
-  release(&bcache.lock);
+  release(&hq->lock);
 }
 
+int
+hashqueue_eq(struct hashqueue *lhs, struct hashqueue *rhs)
+{
+  return &lhs->head == &rhs->head;
+}
+
+void
+hashqueue_init(struct hashqueue *hq)
+{
+  hq->head.prev = &hq->head;
+  hq->head.next = &hq->head;
+  initlock(&hq->lock, "bcache.hashqueue");
+}
+
+void
+hashqueue_addhead(struct hashqueue *hq, struct buf *b)
+{
+  remove_from_hashqueue(b);
+
+  b->next = hq->head.next;
+  b->prev = &hq->head;
+  hq->head.next->prev = b;
+  hq->head.next = b;
+}
+
+uint
+hash(uint value)
+{
+  return value;
+}
+
+struct hashqueue *
+gethashqueue(uint blockno)
+{
+  if(blockno < 0)
+    return 0;
+  uint index = hash(blockno) % NQUEUE;
+  return &bcache.hashqueue[index];
+}
+
+int
+isin_hashqueue(struct buf *b)
+{
+  return b->prev != 0 || b->next != 0;
+}
+
+void
+remove_from_hashqueue(struct buf *b)
+{
+  if(b->next && b->prev){
+    b->next->prev = b->prev;
+    b->prev->next = b->next;
+  }
+  b->next = b->prev = 0;
+}
+
+void
+lockboth(struct spinlock *lhs, struct spinlock *rhs)
+{
+  // reorder lock to avoid deadlock, lhs must hold the lock
+  if(!holding(lhs))
+    panic("lockboth: lhs must hold the lock");
+
+  // already locked
+  if(lhs == rhs)
+    return;
+
+  if(lhs > rhs){
+    // must reorder
+    release(lhs);
+    acquire(rhs);
+    acquire(lhs);
+  } else {
+    // already in order, just lock rhs and return
+    acquire(rhs);
+  }
+}
 
